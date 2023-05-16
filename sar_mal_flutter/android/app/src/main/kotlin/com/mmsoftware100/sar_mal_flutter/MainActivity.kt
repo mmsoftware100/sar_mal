2e316df47cc870a8f714fa9d1caf6e328afe1bb5
Aung Htet <48619157+AungAungHtet2019@users.noreply.github.com>
2023-05-16 10:28:19 +0630

    change format
    change format

---
+++ sar_mal_flutter/android/app/src/main/kotlin/com/mmsoftware100/sar_mal_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mmsoftware100.sar_mal_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ sar_mal_flutter/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:sar_mal_flutter/view/home_page.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home:HomePage(),
+    )
+  );
+}
+
+
