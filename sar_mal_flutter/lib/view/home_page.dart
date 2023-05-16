b469af723c89a89ce748236015b42c0c85ebfcbb
Aung Htet <48619157+AungAungHtet2019@users.noreply.github.com>
2023-05-16 17:13:41 +0630

    a
    a

--- sar_mal_flutter/lib/view/home_page.dart
+++ sar_mal_flutter/lib/view/home_page.dart
@@ -25,6 +25,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey,
       appBar: AppBar(
         backgroundColor: Colors.orange,
         title: Center(child: Text("စားမယ်")),
