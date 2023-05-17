efcd8321d7b242d70d53a218d69a0e95fe0e4002
Aung Htet <48619157+AungAungHtet2019@users.noreply.github.com>
2023-05-17 09:54:09 +0630

    a
    a

---
+++ sar_mal_flutter/lib/view/pages/detail_page.dart
@@ -0,0 +1,67 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+import '../../model/data_model.dart';
+
+class DetailPage extends StatefulWidget {
+  CategoryData categoryData;
+  DetailPage({Key? key,required this.categoryData}) : super(key: key);
+
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: Text(widget.categoryData.title),
+      ),
+      body: ListView(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height / 3.5,
+            width: MediaQuery.of(context).size.width,
+            child: ClipRRect(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
+              ),
+              // child: Image.asset(
+              //   "${widget.img}",
+              //   fit: BoxFit.cover,
+              // ),
+              child: CachedNetworkImage(
+                imageUrl: "${widget.categoryData.imgUrl}",
+                imageBuilder: (context, imageProvider) => Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: imageProvider,
+                      fit: BoxFit.cover,
+                      //colorFilter: ColorFilter.mode(Colors.red, BlendMode.colorBurn)
+                    ),
+                  ),
+                ),
+                placeholder: (context, url) => Padding(
+                  padding: const EdgeInsets.all(100.0),
+                  child: Center(child: CircularProgressIndicator()),
+                ),
+                errorWidget: (context, url, error) => Icon(Icons.error),
+              ),
+            ),
+          ),
+          Text(
+            widget.categoryData.title,
+            style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16),
+          ),
+          Text(
+            widget.categoryData.description,
+            style: TextStyle(fontSize: 14),
+          )
+        ],
+      ),
+    );
+  }
+}
--- sar_mal_flutter/lib/view/pages/selected_category_data_page.dart
+++ sar_mal_flutter/lib/view/pages/selected_category_data_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../model/data_model.dart';
 import '../widgets/trending_item.dart';
+import 'detail_page.dart';
 
 class SelectedCategoryPage extends StatefulWidget {
   DataModel dataModel;
@@ -22,7 +23,12 @@ class _SelectedCategoryPageState extends State<SelectedCategoryPage> {
       ),
       body: ListView(
         children: widget.dataModel.categoryData.map((e) {
-          return TrendingItem(img: e.imgUrl,title: e.title,address: e.description,rating: "5",);
+          return InkWell(
+              child: TrendingItem(img: e.imgUrl,title: e.title,address: e.description,rating: "5",),
+            onTap: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>DetailPage(categoryData: e)));
+            },
+          );
         }).toList(),
       ),
     );
